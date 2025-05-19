8621ddec10620c5c67aa64c0120f75f2709c9d0d
Melksedeque <freelancer@melksedeque.com.br>
2025-05-19 20:00:47 -0300

    Importar MongooseModule com a string de conexão do root no localhost

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   // Decorator @Module() é usado para definir as metadata do módulo.
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:root@localhost:27017/nest?authSource=admin&directConnection=true',
+    ),
+    AssetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
-  imports: [AssetsModule],
 })
 export class AppModule {}

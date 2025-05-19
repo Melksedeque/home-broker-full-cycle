801f10ab192c495ec7299772a1d0c9101ab6252e
Melksedeque <freelancer@melksedeque.com.br>
2025-05-19 20:01:18 -0300

    Importar MongooseModule com nome e schema da classe criada para Assets

--- nestjs-api/src/assets/assets.module.ts
+++ nestjs-api/src/assets/assets.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AssetsService } from './assets.service';
 import { AssetsController } from './assets.controller';
+import { Asset, AssetSchema } from './entities/asset.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Asset.name,
+        schema: AssetSchema,
+      },
+    ]),
+  ],
   controllers: [AssetsController],
   providers: [AssetsService],
 })

7833606fb88a3018362e06c04d264f38594c8518
Melksedeque <freelancer@melksedeque.com.br>
2025-05-20 13:27:35 -0300

    Criação de Resources de Order e endpoints REST

---
+++ nestjs-api/src/orders/dto/create-order.dto.ts
@@ -0,0 +1,10 @@
+import { OrderStatus, OrderType } from '../entities/order.entity';
+
+export class CreateOrderDto {
+  walletId: string;
+  assetId: string;
+  shares: number;
+  price: number;
+  type: OrderType;
+  status: OrderStatus;
+}
